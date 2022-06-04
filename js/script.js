38b08001a2afad00e14b647e16887b87f4d3b9f4
Rafael Henrique Madeira Feitosa <98665149+rafaelhenriquemadeira@users.noreply.github.com>
2022-06-04 15:06:16 -0300

    add modal
    Adicionado modal para login de acesso, ainda tem trabalho a ser feito.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ import initSmoothScroll from "./modules/scroll-suave.js";
 import initAnimaScroll from "./modules/scroll-animation.js";
 import initAcordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
+
 
 initSmoothScroll();
 initAnimaScroll();
 initAcordion();
 initTabNav();
+initModal();
